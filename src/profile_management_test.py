76570955049dbafd4cb29968e1e97d0ebb1b61c6
Hailey Jung <z5213092@unsw.edu.au>
2020-03-09 03:54:45 +1100

    onemoretogo

--- src/profile_management_test.py
+++ src/profile_management_test.py
@@ -1,11 +1,11 @@
-#Ability to view user anyone's user profile, 
-#and modify a user's own profile 
-#(name, email, handle, and profile photo)
+# Ability to view user anyone's user profile, 
+# and modify a user's own profile 
+# (name, email, handle, and profile photo)
 
-#user_profile
-#user_profile_setname
-#user_profile_setemail
-#user_profile_sethandle
+# user_profile
+# user_profile_setname
+# user_profile_setemail
+# user_profile_sethandle
 
 from auth import auth_register
 from user import *
@@ -56,21 +56,24 @@ def test_profile():
 # if the first or last name is more than 100 charactors 
 # it is too long and considered invalid
 
-# invalid_name = "a" * 101
+invalid_name = "a" * 101
 
-# def test_invalid_name(given_name):
-# #     profile = return_profile()
-# #     profile_first_name = profile['user']['name_first']  
-# #     length = len(profile_first_name)
-#     length = len(given_name)    
-#     if length > 100:
-#         raise Exception("Invalid name")
-#     else:
-#         return "Valid name"
+def test_invalid_name(given_name):
+    length = len(given_name)    
+    if length > 100:
+        return "Invalid name"
+    else:
+        return "Valid name"
 
-# def test_name_exception():
-#     with pytest.raises(Exception, match = "Invalid name"):
-#         assert test_invalid_name(invalid_name)
+def test_name_length():
+    user2 = auth_register("hahajk@unless...?", "1234yu!", invalid_name, invalid_name)
+    token = user2['token']
+    uid = user2['u_id'] 
+    profile = user_profile(token, uid)
+    given_first = profile['user']['name_first'] 
+    given_last = profile['user']['name_last']  
+    assert test_invalid_name(given_first) == "Invalid name"
+    assert test_invalid_name(given_last) == "Invalid name"
 
 ###########################################################
 #                test user_profile_setemail                #         
@@ -78,19 +81,24 @@ def test_profile():
 
 # invalid_email = "hahajk@unless...?"
 def check_valid_email(given_email):
-    if re.match(r"(^[a-zA-Z0-9]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", given_email):
+    if re.match(r"(^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$)", given_email):
         return "Valid email"
     else:
         return "Invalid email"
 
 def test_email_exception():
-    user2 = auth_register("hahajk@unless...?", "1234yu!", "Hailey", "Jung")
-    token = user2['token']
-    uid = user2['u_id'] 
+    # assumtion = if i register a user the token and id is all different
+    user3 = auth_register("hahajk@unless...?", "1234yu!", "Hailey", "Jung")
+    token = user3['token']
+    uid = user3['u_id'] 
     profile = user_profile(token, uid)
     given_email = profile['user']['email']
-    assert check_valid_email(given_email) == "Valid email"
+    assert check_valid_email(given_email) == "Invalid email"
 
+
+###########################################################
+#                test user_profile_sethandle              #         
+########################################################### 
 
 
     
