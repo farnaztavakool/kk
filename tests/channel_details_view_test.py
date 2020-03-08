b3f46e71997603e8fd611b08a0d7dff879edb78e
Eric T Chen <z5255650@ad.unsw.edu.au>
2020-03-08 12:33:12 +1100

    Added feature Descriptions

---
+++ tests/channel_details_view_test.py
@@ -0,0 +1,2 @@
+# Feature 5: Within a channel, ability to view all messages, view the members of the channel, and the details of the channel
+
---
+++ tests/message_send_test.py
@@ -0,0 +1,1 @@
+# Feature 6: Within a channel, ability to send a message now, or to send a message at a specified time in the future
