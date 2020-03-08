95c1fd5cc216b374c203acb9d4e567f6ce4890e7
Tim Fan <tfan478@gmail.com>
2020-03-09 09:24:38 +1100

    channel_list_view_test.py:fixed up typos:added more tests to ensure that functions return correct type of data as well

--- tests/channel_list_view_test.py
+++ tests/channel_list_view_test.py
@@ -10,7 +10,7 @@ def get_new_user():
     return (user['u_id'], user['token'])
 
 '''
-testing channels_list()
+testing channels_list() returns the correct number of key/value pairs.
 '''
 
 def test_channels_list_zero_channels(get_new_user):
@@ -58,7 +58,7 @@ def test_channels_list_two_channels_none_authorized(get_new_user):
     channel_leave(token, channel1['channel_id'])
     channel_leave(token, channel2['channel_id'])
     # ensure channels list for dummy user has no channels.
-    my_channels_list = channels_list(token)
+    my_channels_list = channels.channels_list(token)
     assert len(my_channels_list) == 0
 
 def test_channels_list_two_channels_one_authorized(get_new_user):
@@ -90,17 +90,35 @@ def test_channels_list_two_channels_both_authorized(get_new_user):
     assert len(my_channels_list) == 2
 
 '''
-testing channels_listall()
+testing channels_list() returns the correct key/value pairs.
 '''
 
-def test_channels_list_zero_channels(get_new_user):
+def test_channels_list_two_channels_both_authorized_ensure_valid_return(get_new_user):
+    # dummy user.
+    u_id, token = get_new_user
+    # create two channels. assumes creates valid channels.
+    channel1 = channels.channels_create(token, 'channel1', True)
+    channel2 = channels.channels_create(token, 'channel2', True)
+    # join both channels.
+    channel.channel_join(token, channel1['channel_id'])
+    channel.channel_join(token, channel2['channel_id'])
+    # ensure channels list for dummy user has both channels.
+    my_channels_list = channels.channels_list(token)
+    assert my_channels_list['channel1'] == channel1['channel_id']
+    assert my_channels_list['channel2'] == channel2['channel_id']
+
+'''
+testing channels_listall() returns the correct number of key/value pairs
+'''
+
+def test_channels_listall_zero_channels(get_new_user):
     # dummy user.
     u_id, token = get_new_user
     # ensure channels list for dummy user has no channels.
     my_channels_list = channels.channels_list(token)
     assert len(my_channels_list) == 0
 
-def test_channels_list_one_channels_none_authorized(get_new_user):
+def test_channels_listall_one_channels_none_authorized(get_new_user):
     # dummy user.
     u_id, token = get_new_user
     # create one channels. assumes creates valid channels.
@@ -110,10 +128,10 @@ def test_channels_list_one_channels_none_authorized(get_new_user):
     # leave channel.
     channel.channel_leave(token, channel2['channel_id'])
     # ensure channels list for dummy user has all channels.
-    my_channels_list = channels.channels_list(token)
+    my_channels_list = channels.channels_listall(token)
     assert len(my_channels_list) == 1
 
-def test_channels_list_one_channels_one_authorized(get_new_user):
+def test_channels_listall_one_channels_one_authorized(get_new_user):
     # dummy user.
     u_id, token = get_new_user
     # create one channels. assumes creates valid channels.
@@ -121,11 +139,11 @@ def test_channels_list_one_channels_one_authorized(get_new_user):
     # join channel
     channel.channel_join(token, channel1['channel_id'])
     # ensure channels list for dummy user has all channels.
-    my_channels_list = channels.channels_list(token)
+    my_channels_list = channels.channels_listall(token)
     assert len(my_channels_list) == 1
 
 
-def test_channels_list_two_channels_none_authorized(get_new_user):
+def test_channels_listall_two_channels_none_authorized(get_new_user):
     # dummy user.
     u_id, token = get_new_user
     # create two channels. assumes creates valid channels.
@@ -138,10 +156,10 @@ def test_channels_list_two_channels_none_authorized(get_new_user):
     channel_leave(token, channel1['channel_id'])
     channel_leave(token, channel2['channel_id'])
     # ensure channels list for dummy user has all channels.
-    my_channels_list = channels_list(token)
+    my_channels_list = channels.channels_listall(token)
     assert len(my_channels_list) == 2
 
-def test_channels_list_two_channels_one_authorized(get_new_user):
+def test_channels_listall_two_channels_one_authorized(get_new_user):
     # dummy user.
     u_id, token = get_new_user
     # create two channels. assumes creates valid channels.
@@ -153,10 +171,10 @@ def test_channels_list_two_channels_one_authorized(get_new_user):
     # leave one channel.
     channel.channel_leave(token, channel1['channel_id'])
     # ensure channels list for dummy user has all channels.
-    my_channels_list = channels.channels_list(token)
+    my_channels_list = channels.channels_listall(token)
     assert len(my_channels_list) == 2
 
-def test_channels_list_two_channels_both_authorized(get_new_user):
+def test_channels_listall_two_channels_both_authorized(get_new_user):
     # dummy user.
     u_id, token = get_new_user
     # create two channels. assumes creates valid channels.
@@ -166,5 +184,23 @@ def test_channels_list_two_channels_both_authorized(get_new_user):
     channel.channel_join(token, channel1['channel_id'])
     channel.channel_join(token, channel2['channel_id'])
     # ensure channels list for dummy user has all channels.
-    my_channels_list = channels.channels_list(token)
+    my_channels_list = channels.channels_listall(token)
     assert len(my_channels_list) == 2
+
+'''
+testing channels_listall() returns the correct key/value pairs.
+'''
+
+def test_channels_listall_two_channels_both_authorized_ensure_valid_return(get_new_user):
+    # dummy user.
+    u_id, token = get_new_user
+    # create two channels. assumes creates valid channels.
+    channel1 = channels.channels_create(token, 'channel1', True)
+    channel2 = channels.channels_create(token, 'channel2', True)
+    # join both channels.
+    channel.channel_join(token, channel1['channel_id'])
+    channel.channel_join(token, channel2['channel_id'])
+    # ensure channels list for dummy user has both channels.
+    my_channels_list = channels.channels_listall(token)
+    assert my_channels_list['channel1'] == channel1['channel_id']
+    assert my_channels_list['channel2'] == channel2['channel_id']
