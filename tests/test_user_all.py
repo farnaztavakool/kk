09ec1c6b5392ef6879d9f4a240ed25ab2634b0bd
Farnaz Tavakol <tavakolfarnaz@gmail.com>
2020-03-09 18:22:17 +1100

    added some comments

--- tests/test_user_all.py
+++ tests/test_user_all.py
@@ -2,6 +2,8 @@ from auth import auth_register
 import pytest
 from other import users_all
 
+
+# this test file will should all of the users registered
 def test_all_user():
 
     usr = auth_register("tavakolfarnaz@gmail.com","0312138","farnaz","tavakol")
