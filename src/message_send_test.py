4abaad5e18ef53653e77556c1aa45f590d9f904c
Eric T Chen <z5255650@ad.unsw.edu.au>
2020-03-11 15:19:40 +1100

    fixed up bug in messagee_send_test

--- src/message_send_test.py
+++ src/message_send_test.py
@@ -1,8 +1,8 @@
 # Feature 6: Within a channel, ability to send a message now, or to send a message at a specified time in the future
 
-from datetime import datetime, timedelta
+import datetime
+from message import message_send
 import pytest
-import message
 import auth
 from error import InputError
 from error import AccessError
@@ -25,9 +25,9 @@ def channel_id(authUser):
         
 # Test if a member sends messages    
 def test_message_send_valid(authUser, unAuthUser, channel_id):
-    message.message_send(authUser['token'], channel_id, "Message 1")
-    message.message_send(authUser['token'], channel_id, "Message 2")
-    message.message_send(authUser['token'], channel_id, "Message 3!")
+    message_send(authUser['token'], channel_id, "Message 1")
+    message_send(authUser['token'], channel_id, "Message 2")
+    message_send(authUser['token'], channel_id, "Message 3!")
     assert channel.channel_messages(authUser['token'], channel_id, 0)['messages'][0]['message'] == "Message 3!"
     assert channel.channel_messages(authUser['token'], channel_id, 0)['messages'][1]['message'] == "Message 2"
     assert channel.channel_messages(authUser['token'], channel_id, 0)['messages'][2]['message'] == "Message 1"
@@ -35,33 +35,33 @@ def test_message_send_valid(authUser, unAuthUser, channel_id):
 # Test if an unauthorized member sends messages
 def test_message_send_invalid_user(authUser, unAuthUser, channel_id):
     with pytest.raises(AccessError):
-        message.message_send(unAuthUser['token'], channel_id, "I am invalid")
+        message_send(unAuthUser['token'], channel_id, "I am invalid")
 
 # Test if message is too long
-def test_message_send_invalid_message_length(authUser, unAuthUser, channel_id):
+def test_message_send_too_long(authUser, unAuthUser, channel_id):
     with pytest.raises(InputError):
-       message.message_send(authUser['token'], channel_id, "Mess"*5000)
+       message_send(authUser['token'], channel_id, "Mess"*5000)
        
 # Test if user sends message in the future
 def test_message_sendlater_success(authUser, unAuthUser, channel_id):
     timeSent = datetime.datetime.now(datetime.timezone.utc).astimezone().timestamp()+0.1
-    message_id = message_sendlater(authUser['token'], channel_id, "Message 4", timeSent)        
+    message_id = message_send(authUser['token'], channel_id, "Message 4")        
 
 # Test if user sends message that is too long in the future
 def test_message_sendlater_too_long(authUser, unAuthUser, channel_id):
     timeSent = datetime.datetime.now(datetime.timezone.utc).astimezone().timestamp()+1
     with pytest.raises(InputError):
-        message_sendlater(authUser['token'], channel_id, "Mess"*5000, timeSent)
+        message_send(authUser['token'], channel_id, "Mess"*5000)
 
 # Test if unauthorized user sends message in the future
 def test_message_sendlater_unauthorized(authUser, unAuthUser, channel_id):
     timeSent = datetime.datetime.now(datetime.timezone.utc).astimezone().timestamp()+1
     with pytest.raises(AccessError):
-        message_sendlater(unAuthUser['token'], channel_id, "Message 5", timeSent)
+        message_send(unAuthUser['token'], channel_id, "Message 5")
         
 # Test if user sends message in the past
 def test_message_sendlater_past(authUser, unAuthUser, channel_id):
     timeSent = datetime.datetime.now(datetime.timezone.utc).astimezone().timestamp()-0.5
     with pytest.raises(InputError):
-        message_sendlater(authUser['token'], channel_id, "Message 6", timeSent)
+        message_send(authUser['token'], channel_id, "Message 6")
 
