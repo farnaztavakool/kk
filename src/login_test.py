fa0cc863d206508808ab239df23c4e4c37c331f4
Farnaz Tavakol <f.tavakol@student.unsw.edu.au>
2020-03-11 13:21:56 +1100

    Update login_test.py

--- src/login_test.py
+++ src/login_test.py
@@ -14,22 +14,27 @@ def test_register_works():
     auth_register(email,password,fname,lname)
     auth_login(email,password)
 
+# cheking if the function returns error when registering with an unvalid email
 def test_register():
     with pytest.raises(InputError):
         assert auth_register("123","12345","fl","th")
 
+# checking function returns error when using short password
 def test_password():
 
     with pytest.raises(InputError):
         assert auth_register(email,"123",fname,lname)
-
+        
+# checking function returns error when using long fname
 def test_long_first_name():
     with pytest.raises(InputError):
         assert auth_register(email,password,longname,lname)
-
+        
+# checking function returns error when using long lname
 def test_long_last_name():
     with pytest.raises(InputError):
         assert auth_register(email,password,fname,longname)
+        
 #token passes to us from login == register
 
 def test_token():
@@ -39,28 +44,28 @@ def test_token():
     assert user_id2 == user_id1
     assert user_token1 == user_token2
 
-#checking the registered email does not belong to a user
+#checking the function throwing an error when registering with an email already registered 
 def test_check_user_email():
     #register a user with email
     auth_register(email,password,fname,lname)
     # should raises error for registering the same email again
     with pytest.raises(InputError):
         assert auth_register(email,"1111111","jack","tom")
-
+        
+        
+#checking the function raise error when entered the
+   
 def test_user_pass():
-    
-    #the function should raise error when entered the
-    #wrong password
     auth_register(email,password,fname,lname)
     with pytest.raises(InputError):
         assert auth_login(email,"123")
 
-# entering an unvalid email
+# checking the function returns an error entering an unvalid email
 def test_user_login_email():
     with pytest.raises(InputError):
         assert [auth_register(email,password = "12345678910",name_first = "farnaz",name_last = "jade") for email in li]
        
-#each user having a unique toke and id 
+#checking each user having a unique token and id 
 def check_unique_token():
 
     user1_token,user1_id = auth_register(email,password,fname,lname)
@@ -68,7 +73,7 @@ def check_unique_token():
     assert user1_token != user2_token
     assert user1_id != user2_id
 
-# should return true if the user is active
+# checking the logout function works when the user is active
 def test_auth_logout():
 
     auth_register(email,password,fname,lname)
@@ -80,7 +85,7 @@ def test_auth_logout():
     assert k['is_success'] == True
 
 
-# raising exception when the token is not active
+#raising exception when the token is not active
 
 def test_unactive_logout():
     auth_register(email,password,fname,lname)
