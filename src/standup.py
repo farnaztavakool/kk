d990dde1fc00e202d2ff41eab24bb71496d82d31
Tim Fan <tfan478@gmail.com>
2020-03-28 02:44:16 +1100

    initialized standup routes and functions

---
+++ src/standup.py
@@ -0,0 +1,10 @@
+import storage
+import message
+import helper
+
+def standup_start(token, channel_id, length):
+    return
+def standup_active(token, channel_id):
+    return
+def standup_send(token, channel_id, message):
+    return
