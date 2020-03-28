23511e73ed5706acce4bc50d74e882b63a81828a
Tim Fan <tfan478@gmail.com>
2020-03-28 19:49:50 +1100

    run into an issue where its a bit hard to find out if a user is or is not part of a given channel

--- src/standup.py
+++ src/standup.py
@@ -2,6 +2,7 @@ import storage
 import message
 import helper
 import datetime
+from error import InputError, AccessError
 
 def standup_start(token, channel_id, length):
     channel_all = storage.load_channel_all()
@@ -27,6 +28,7 @@ def standup_start(token, channel_id, length):
     standup['time_finish'] = time_finish
 
     ### sleep for length seconds (during this period standup_send() calls will be successful).
+    ### < we assume that the above lines are done almost instaneously :)))) >
     time.sleep(length)
 
     ### stop standup.
@@ -42,6 +44,35 @@ def standup_active(token, channel_id):
     ### ERROR: channel_id is not a valid channel.
     if channel_id not in channel_all:
         raise InputError()
-    return {}
+
+    ### finding is_active.
+    channel = channel_all[channel_id]
+    standup = channel['standup']
+    is_active = channel['is_active']
+
+    ### finding time_finish.
+    if is_active == True:
+        time_finish = standup['time_finish']
+    else:
+        time_finish = None
+
+    return {
+        'is_active': is_active,
+        'time_finish': time_finish,
+    }
 def standup_send(token, channel_id, message):
+    channel_all = storage.load_channel_all()
+    ### ERROR: channel_id is not a valid channel.
+    if channel_id not in channel_all:
+        raise InputError()
+    channel = channel_all[channel_id]
+    standup = channel['standup']
+    ### ERROR: an active standup is not currently running in this channel
+    if standup['is_active'] == False:
+        raise InputError()
+    ### ERROR: message is more than 1000 characters.
+    if len(message) > 1000:
+        raise InputError()
+    ### ERROR: user is not a member of the channel that the standup is occurring in.
+    user_all = storage.load_user_all()
     return {}
