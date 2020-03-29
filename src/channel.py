1cb9d2443db6167158490c2de077089de5f5baa7
Hailey Jung <z5213092@unsw.edu.au>
2020-03-29 20:36:40 +1100

    created remove owner

--- src/channel.py
+++ src/channel.py
@@ -1,6 +1,35 @@
-import storage
 import helper
-from error import InputError, AccessError
+import storage
+import auth
+import error
+
+def get_data():
+    try:
+        data = storage.load_channel_all()
+    except Exception:
+        storage.new_storage()
+        data = storage.load_channel_all()
+    return data
+
 
 def channel_addowner(token,channel_id,u_id):
-    return 
+   "Make user with user id u_id an owner of this channel"
+
+    # Error check that channel_id refers to a valid channel
+    check_channel(channel_id, data)
+
+    # Error check if the user is already an owner
+    channel_data = get_data()
+    data_user = auth.get_data()
+    if data_user[u_id] in channel_data[channel_id]['owner']:
+        raise error.InputError
+
+    # Error if the authorised user is not already a member of the channel
+    channel_data = get_data()
+    helper.check_channel(channel_id, channel_data)
+    helper.check_access(token,channel_data, channel_id)
+
+    # Add to owner list
+    #channel.add_owners(u_id, info['name_first'], info['name_last'], info['profile_img_url'])
+
+def channel_remove(token,channel_id,u_id):
