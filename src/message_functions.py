8d05653a2e9bc16c49019aa6fc8559f4fbd9bf00
Hailey Jung <z5213092@unsw.edu.au>
2020-03-30 03:15:51 +1100

    sends message after a set time

--- src/message_functions.py
+++ src/message_functions.py
@@ -31,17 +31,22 @@ def message_send(token,channel_id,message):
     # channel['messages_list'].prepend(message_data)
     return {'message_id': message_id} 
 
-def message_send(token,channel_id,message):
+def message_sendlater(token, channel_id, message, time_sent):
 
     data = storage.load_channel_all()
     user_data = auth.get_data()
     u_id = helper.get_id(token, user_data)
-    helper.check_access(u_id,data,channel_id)
+
     message_id = helper.channel_id()
     # helper.check_access(u_id,data, channel_id)
     if len(message) > 1000:
         raise InputError("Message should be under 1000 characters.")
-  
+
+    current_time = get_timestamp()
+    if time_sent < current_time:
+        raise InputError('The time entered is a time in the past')
+
+    helper.check_access(u_id,data,channel_id)
 
     message_data = {
         'message_id': message_id,
@@ -51,6 +56,11 @@ def message_send(token,channel_id,message):
         'message_text': message,
     }
     
+    time = time_sent - get_timestamp()
+    timer = threading.Timer(interval, storage.add_message(message_data, channel_id), \
+                            [message_data, channel_id])
+    timer.start()
+
     storage.add_message(message_data, channel_id)
     # channel['messages_list'].prepend(message_data)
     return {'message_id': message_id} 
