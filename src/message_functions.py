f9928670cd1c9ccfd5b1f40bf619e0314da29099
Tim Fan <tfan478@gmail.com>
2020-03-30 00:38:02 +1100

    completed message_pin and message_unpin. method of finding message is incredibly slow LOL

--- src/message_functions.py
+++ src/message_functions.py
@@ -29,3 +29,29 @@ def message_send(token,channel_id,message):
     storage.add_message(message_data, channel_id)
     # channel['messages_list'].prepend(message_data)
     return {'message_id': message_id} 
+
+# WHY DO THEY NOT INPUT THE CHANNEL_ID REEE.
+def message_pin(token,message_id):
+    channels_all = storage.load_channel_all()
+    # find the message dictionary with message_id message_id.
+    # OH GOD OH FUCK WHAT AM I WRITING. (wait its not that bad).
+    for channel in channels_all:
+        messages = channel['messages']
+        for message in messages:
+            if message['message_id'] == message_id:
+                if message['is_pinned'] == True:
+                    raise InputError()
+                message['is_pinned'] = True
+    return {}
+
+def message_unpin(token,message_id):
+    channels_all = storage.load_channel_all()
+    # find the message dictionary with message_id message_id..
+    for channel in channels_all:
+        messages = channel['messages']
+        for message in messages:
+            if message['message_id'] == message_id:
+                if message['is_pinned'] == False:
+                    raise InputError()
+                message['is_pinned'] = False
+    return {}
