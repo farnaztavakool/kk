f285704a6fe87d9f0c016406ab05ebdff0c5f5e1
Hailey Jung <z5213092@unsw.edu.au>
2020-03-30 21:14:31 +1100

    added tims functions

--- src/message_functions.py
+++ src/message_functions.py
@@ -31,6 +31,45 @@ def message_send(token,channel_id,message):
     # channel['messages_list'].prepend(message_data)
     return {'message_id': message_id} 
 
+
+# WHY DO THEY NOT INPUT THE CHANNEL_ID REEE.
+def message_pin(token,message_id):
+    channels_all = storage.load_channel_all()
+    message = helper.get_message_dictionary(message_id,channels_all)
+    if message['is_pinned'] == True:
+        raise InputError()
+    message['is_pinned'] = True
+    storage.save_channel_all(channels_all)
+    return {}
+
+def message_unpin(token,message_id):
+    channels_all = storage.load_channel_all()
+    message = helper.get_message_dictionary(message_id,channels_all)
+    if message['is_pinned'] == False:
+        raise InputError()
+    message['is_pinned'] = False
+    storage.save_channel_all(channels_all)
+    return {}
+
+def message_remove(token,message_id):
+    channels_all = storage.load_channel_all()
+    messages = helper.get_messages_list_containing_message(message_id,channels_all)
+    message = helper.get_message_dictionary(message_id,channels_all)
+    ### ERROR: message no longer exists.
+    if message == {}:
+        raise InputError()
+    # remove message from the messages list.
+    messages.remove(message)
+    storage.save_channel_all(channels_all)
+    return {}
+
+def message_edit(token,message_id,message):
+    channels_all = storage.load_channel_all()
+    message_dict = helper.get_message_dictionary(message_id,channels_all)
+    message_dict['message_text'] = message 
+    storage.save_channel_all(channels_all)
+    return {}
+
 def message_sendlater(token, channel_id, message, time_sent):
 
     data = storage.load_channel_all()
@@ -67,17 +106,8 @@ def message_sendlater(token, channel_id, message, time_sent):
                             [message_data, channel_id])
     timer.start()
 
-    # storage.add_message(message_data, channel_id)
-    # channel['messages_list'].prepend(message_data)
+
     return {'message_id': message_id} 
 
-# def message_react(token, message_id, react_id):
-#     channels_all = storage.load_channel_all()
-#     message = helper.get_message_dictionary(message_id,channels_all)
-#     if message['reacts'] == True:
-#         raise InputError()
-#     message['reacts'] = True
-#     storage.save_channel_all(channels_all)
-#     return {}
 
 
