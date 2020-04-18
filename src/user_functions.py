fef845b1aac16dfcbb6456e3a869ba5e3ed63051
Tim Fan <tfan478@gmail.com>
2020-04-19 07:47:25 +1000

    WTF DID I DO, IM RETARDED

--- src/user_functions.py
+++ src/user_functions.py
@@ -1,6 +1,12 @@
 import storage
 import error
 import helper
+import requests
+import shutil
+import random
+import string
+from PIL import Image
+import os
 
 
 # why is git so weird. 
@@ -17,6 +23,7 @@ def user_profile(token, u_id):
         'name_first': user_all[str(u_id)]['name_first'],
         'name_last': user_all[str(u_id)]['name_last'],
         'handle_str': user_all[str(u_id)]['token'],
+        'profile_img_url': user_all[str(u_id)]['profile_img_url'],
     }
     return user
     
@@ -64,6 +71,7 @@ def users_all(token):
             'name_first': user_all[user]['name_first'],
             'name_last': user_all[user]['name_last'],
             'handle_str': user_all[user]['handle'],
+            'profile_img_url': user_all[user]['profile_img_url'],
         }
         users["users"].append(user_data)
     return users
@@ -142,5 +150,7 @@ def user_profiles_uploadphoto(token, img_url, x_start, y_start, x_end, y_end):
                 member['profile_img_url'] = profile_img_url
     storage.save_channel_all(channel_all)
     return {}
-    
+def get_random_alphaNumeric_string(stringLength):
+    lettersAndDigits = string.ascii_letters + string.digits
+    return ''.join((random.choice(lettersAndDigits) for i in range(stringLength)))
 
